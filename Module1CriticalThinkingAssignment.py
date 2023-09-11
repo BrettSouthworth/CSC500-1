4ff655df51c79967560157a857d5f70eeef98728
Brett Southworth <bsouthworthumd@gmail.com>
2023-09-11 16:47:51 -0400

    Module 1

---
+++ Module1CriticalThinkingAssignment.py
@@ -0,0 +1,15 @@
+print("Welcome to Bretts basic calculator. Please enter 2 numbers:")
+num1 = float(input())
+num2 = float(input())
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print("~~~~~")
+print("Results:")
+print("~~~~~")
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+print("~~~~~")
